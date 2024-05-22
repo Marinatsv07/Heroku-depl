cc1b770fcbf3414a05990f29db957014f7ece5a9
Marina Tsvetkova <61690238+Marinatsv07@users.noreply.github.com>
2024-05-22 17:17:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -51,6 +51,9 @@
     
         return render_template('index.html', prediction_text=prediction_text)
     return app
+
+
+     
     if __name__ == "__main__":
         port = int(os.environ.get("PORT", 5000))
         app.run(debug=True, host='0.0.0.0', port=port)
